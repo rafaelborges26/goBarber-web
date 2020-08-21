c1279162ab10d19c9a1f8813381df3c7c25fa5f7
= <rafael.borges2698@gmail.com>
2020-08-21 03:17:16 -0300

    adding the html and css of page sigin

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,8 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-    margin: 0px;
-    padding: 0px;
+    margin: 0;
+    padding: 0;
     box-sizing: border-box;
     outline: 0;
 
