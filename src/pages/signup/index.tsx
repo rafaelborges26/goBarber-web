149f4e3e4cf8c570e562096825d538a75b6619c6
= <rafael.borges2698@gmail.com>
2020-08-25 04:35:34 -0300

    add the unform for form

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,43 +1,51 @@
 import React from 'react'
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi'
+import { Form } from '@unform/web'
 
 import logoImg from '../../assets/logo.svg'
-
 import { Container, Content, Background } from './styles'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
 
-const SignOut: React.FC = () => (
-    <>
-    <Container>
+const SignOut: React.FC = () => {
+    
+    function handleSubmit(data: object) {
+        console.log(data)
+    }   
 
+return (
+         <>
+    <Container>
+    
     <Background />
-     
+    
         <Content>
             <img src={logoImg} alt="GoBarber"/>
-
-            <form>
+    
+            <Form onSubmit={handleSubmit}>
                 <h1>Faça seu cadastro</h1>
-
+    
                 <Input name="name"  icon={FiUser} placeholder="Nome"/>
                 <Input name="email" icon={FiMail} placeholder="E-mail"/>
                 
                 <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
-
+    
                 <Button type="submit">Cadastrar</Button>
                 
-            </form>
-
+            </Form>
+    
         <a href="create">
             <FiArrowLeft />
             Voltar login
         </a>
-
+    
         </Content>
-
+    
     </Container>
     </>
-)
+    )
+
+}
 
 export default SignOut
